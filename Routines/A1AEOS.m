9fbf9836e44cf99ff7e426731f40c61566804a77
Sam Habiel <sam.habiel@gmail.com>
2014-05-10 19:44:16 -0400

    Changes to make the KIDS VC exporter work
    1. With KIDS
    2. On Windows
    
    KIDS hook not done yet.

--- Routines/A1AEOS.m
+++ Routines/A1AEOS.m
@@ -1,25 +1,37 @@
-A1AEOS	; VEN/SMH - Patch Module Operating System Interface;2014-03-31  1:27 PM
-	;;2.4;PATCH MODULE;;Mar 28, 2014
-	;
-	; This routine is not SAC compliant
-	;
-MKDIR(DIR)	; [PUBLIC] - $$; mkdir DIR name. Unix output for success and failure.
-	N CMD S CMD="mkdir -p '"_DIR_"'" ; mk sure that we take in account spaces
-	N OUT ; Exit value of command.
-	I +$SY=47 D  ; GT.M
-	. O "p":(shell="/bin/sh":command=CMD)::"pipe" U "p" C "p"
-	. I $ZV["V6.1" S OUT=$ZCLOSE ; GT.M 6.1 only returns the status!!
-	. E  S OUT=0
-	I +$SY=0 S OUT=$ZF(-1,CMD) ; Cache
-	QUIT OUT
-	;
-T2	; @TEST Make a directory
-	N % S %=$$MKDIR("/tmp/test/sam")
-	D CHKEQ^XTMUNIT(%,0,"Status of mkdir should be zero")
-	N % S %=$$MKDIR("/lksjdfkjsdf/")
-	D CHKEQ^XTMUNIT(%,1,"Status of failed mkdir should be one")
-	QUIT
-	;
+A1AEOS ; VEN/SMH - Patch Module Operating System Interface;2014-03-31  1:27 PM
+ ;;2.4;PATCH MODULE;;Mar 28, 2014
+ ;
+ ; This routine is not SAC compliant
+ ;
+MKDIR(DIR) ; [PUBLIC] - $$; mkdir DIR name. Unix output for success and failure.
+ ; Construct Command
+ N CMD S CMD="mkdir -p '"_DIR_"'" ; mk sure that we take in account spaces ; This is for Unix!!!
+ I +$SY=0,$ZVERSION(1)=2 S CMD="mkdir """_DIR_"""" ; Cache/Windows
+ ;
+ N D S D=$$D() ; File system delimiter
+ I $E(DIR,$L(DIR))=D S DIR=$E(DIR,1,$L(DIR)-1) ; Remove trailing slash (forward or backwards). $ZSEARCH cares!
+ ;
+ ; Special Case for Cache/Windows
+ I +$SY=0,$ZSEARCH(DIR)'="" QUIT 0 ; Cache and directory exists; mkdir on Windows returns 1 
+ ;
+ ;
+ N OUT ; Exit value of command.
+ I +$SY=47 D  ; GT.M
+ . O "p":(shell="/bin/sh":command=CMD)::"pipe" U "p" C "p"
+ . I $ZV["V6.1" S OUT=$ZCLOSE ; GT.M 6.1 only returns the status!!
+ . E  S OUT=0
+ I +$SY=0 S OUT=$ZF(-1,CMD) ; Cache Windows and Unix
+ QUIT OUT
+ ;
+T2 ; @TEST Make a directory
+ N D S D=$$D() ; Delimiter
+ N % S %=$$MKDIR(D_"tmp"_D_"test"_D_"sam")
+ D CHKEQ^XTMUNIT(%,0,"Status of mkdir should be zero")
+ I +$SY=0,$ZVERSION(1)=2 QUIT  ; Next test cannot be done in Windows b/c Cache runs as Admin.
+ N % S %=$$MKDIR(D_"lksjdfkjsdf"_D)
+ D CHKEQ^XTMUNIT(%,1,"Status of failed mkdir should be one")
+ QUIT
+ ;
 PWD() ; [PUBLIC] $$ - Current directory
  I +$SY=47 Q $ZD
  I +$SY=0 Q $ZU(168)
